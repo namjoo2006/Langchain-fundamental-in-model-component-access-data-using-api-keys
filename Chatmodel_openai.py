3b8f9d536acf0a6e294188fa35e991b678a1bb59
Aniket Waghmare <124170359+waghmareaniket@users.noreply.github.com>
2025-08-05 22:17:25 +0530

    Add files via upload

---
+++ Chatmodel_openai.py
@@ -0,0 +1,9 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model=ChatOpenAI(model='gpt-4',temperature=1.5 ,max_completion_tokens=10)
+
+result=model.invoke('what is the capital of india')
+print(result.content)
---
+++ Claude_antropic.py
@@ -0,0 +1,7 @@
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+model=ChatAnthropic(model="claude-3-5-sonnet-20241022")
+result=model.invoke('what is deep learning')
+print(result.content)
